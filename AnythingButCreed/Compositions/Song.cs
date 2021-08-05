a85bf28ef0657802cb4dfdeb665865874b97d26f
John Maple <jmaple@bellsouth.net>
2021-08-04 20:55:04 -0500

    Add project files.

---
+++ AnythingButCreed/Compositions/Song.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnythingButCreed.Compositions
+{
+    class Song
+    {
+        public string Artist { get; set; }
+        public string Name { get; set; }
+        public string Composer { get; set; }
+
+        public Song(string composer, string artist, string name)
+        {
+            Artist = artist;
+            Name = name;
+            Composer = composer;
+        }
+    }
+}
---
+++ AnythingButCreed/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using AnythingButCreed.Compositions;
+using System.Collections.Generic;
+
+namespace AnythingButCreed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Song> allSongs = new List<Song>();
+            List<Song> goodSongs = new List<Song>();
+
+            Console.WriteLine("\n\n     Anything but Creed!\n");
+            var bobby = new Song("Kris Kristofferson", "Janice Joplin", "Me and Bobby McGee");
+            var baba = new Song("Pete Townshend", "The Who", "Baba O'Riley");
+            var breath = new Song("Mark Tremonti and Scott Stapp", "Creed", "One Last Breath");
+            var sacrifice = new Song("Mark Tremonti and Scott Stapp", "Creed", "My Sacrifice");
+            var arms = new Song("Mark Tremonti and Scott Stapp", "Creed", "With Arms Wide Open");
+            var higher = new Song("Mark Tremonti and Scott Stapp", "Creed", "Higher");
+            var woodstock = new Song("Joni Mitchell", "Joni Mitchell", "Woodstock");
+            var something = new Song("Chriss Klafford", "Chris Klafford", "Something Like Me");
+            var purple = new Song("Jimi Hendrix", "Jimi Hendrix", "Purple Haze");
+            var morgens = new Song("Robert Schumann", "Benita Valente", "Mondnacht");
+            var blume = new Song("Robert Schumann", "Benita Valente", "Die Blume der Ergebung");
+            var soul = new Song("Carlos Santana et al", "Santana", "Soul Sacrifice");
+
+            allSongs.Add(arms);
+            allSongs.Add(bobby);
+            allSongs.Add(baba);
+            allSongs.Add(breath);
+            allSongs.Add(woodstock);
+            allSongs.Add(something);
+            allSongs.Add(purple);
+            allSongs.Add(sacrifice);
+            allSongs.Add(morgens);
+            allSongs.Add(higher);
+            allSongs.Add(blume);
+            allSongs.Add(soul);
+
+            int i = 1;
+            Console.WriteLine("     Here is a list of 12 songs\n");
+            allSongs.ForEach(song => Console.WriteLine($"     {i++}. \"{song.Name}\" by {song.Composer}, performed by {song.Artist}"));
+            Console.Write('\n');
+
+            Console.WriteLine("     Now lets remove all the songs by Creed.");
+            allSongs.ForEach(song => goodSongs.Add(song));
+            goodSongs.RemoveAll(song => song.Artist == "Creed");
+
+            i = 1;
+            Console.WriteLine("     OK, here are the songs without Creed - \"Anything but Creed!\"\n");
+            goodSongs.ForEach(song => Console.WriteLine($"     {i++}. \"{song.Name}\" by {song.Composer}, performed by {song.Artist}"));
+            Console.Write('\n');
+        }
+    }
+}
